88c7ab1e779e64570bf0519311ae576b22b52d00
Waine <martinzuzic@gmail.com>
2021-06-21 16:11:10 +0200

    Project started

---
+++ cypress/integration/bistro.spec.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+import {BistroPage} from "../page-bjects/bistro-page";
+
+describe('bistro order', () => {
+    const bistroPage = new BistroPage()
+
+    const location = 'Bratislava'
+    const address = location       //'Bratislava - Petržalka, Bradáčova 1683/1'
+    const foodGroup = 'Drinks'
+
+
+    it('pick location', () => {
+
+        bistroPage.navigate('/')
+        bistroPage.pickLocation(location)
+        bistroPage.validatePageForLocation(location)
+
+    })
+
+    it('pick food group', () => {
+
+        bistroPage.pickFoodGroup(foodGroup)
+        bistroPage.validatePageForFoodGroup(foodGroup, location)
+        
+    })
+
+    it('pick restaurant', () => {
+
+        // bistroPage.navigate('/donaska-napoje/bratislava/')
+
+        bistroPage.setFilter('Rating')
+        bistroPage.pickRestaurant(1)
+        bistroPage.validatePageOfRestaurant()
+      
+    })
+
+    it('pick food', () => {
+
+        bistroPage.navigate('/domased/')
+
+        bistroPage.pickFoodFromMenu(1)
+        bistroPage.enterTheAddress(address)
+        bistroPage.pickFoodFromMenu(2)
+        bistroPage.pickFoodFromMenu(3)
+        bistroPage.validateItemsInCart(3)
+
+    })
+})
---
+++ cypress/page-bjects/bistro-page.js
@@ -0,0 +1,59 @@
+export class BistroPage {
+
+    navigate(url) {
+        cy.visit(url)
+    }
+
+    pickLocation(location) {
+        cy.get('#hp-autocomplete').type(location)
+        cy.get('.results > :nth-child(1)').click()
+    }
+
+    validatePageForLocation(location) {
+        cy.get('h1').should('contain', 'Food delivery '+location)
+    }
+
+    pickFoodGroup(foodGroup) {
+        cy.contains(foodGroup).click()
+    }
+
+    validatePageForFoodGroup(foodGroup, location) {
+        cy.get('h1').should('contain', foodGroup).and('contain', location)
+    }
+
+    setFilter(typeOfSorting) {
+        cy.get('.selectButtonTitle').click()
+        cy.contains(typeOfSorting).click()
+        cy.get('.restaurantFilter > .button').click()
+        cy.get('#filterFreeDelivery').click()
+        cy.get('#filterOnlinePayment').click()
+        cy.get('#filterHomeDelivery').click()
+    }
+
+    pickRestaurant(rankingOfRestaurant) {
+        cy.get('.content > .action > .button').eq(rankingOfRestaurant - 1).click()
+    }
+
+    validatePageOfRestaurant() {
+        cy.get('.cartTitle').should('contain', 'Your order')
+    }
+
+    pickFoodFromMenu(orderInMenu) {
+        cy.get(':nth-child(1) > .content-wrapper').eq(orderInMenu - 1).click()
+    }
+
+    enterTheAddress(address) {
+        cy.window().then(win => {
+
+            cy.get('.bistro-input__input').type(address) 
+            cy.get(':nth-child(1) > .location-results__result').contains(address).click({force: true}) //- Petržalka, Bradáčova 1683/1').click({force: true})
+            cy.get('.specification-wrapper__footer--submit').click({force: true})
+            
+        })
+        cy.get('#cartDataContainer > .items > li')
+    }
+
+    validateItemsInCart(countOfItems) {
+        cy.get('#cart-fixed > .inside').find('.name').should('have.length', countOfItems)
+    }
+}
